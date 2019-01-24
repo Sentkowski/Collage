ec6e4eb5cff941fede642b487a56f9fc523a763d
Szymon Sentkowski <szysen123@gmail.com>
2019-01-24 18:22:52 +0100

    Fixed animations between sections

--- javascript/my_scripts.js
+++ javascript/my_scripts.js
@@ -1,46 +1,54 @@
 $(document).ready(function() {
+
     $( ".proceed_button" ).click(function() {
-        hideWelcome();
+        fromWelcomeToInstructions();
     });
+
+    $( ".instructions_button" ).click(function() {
+        hideCollage();
+    });
+
 });
 
-function hideWelcome() {
-    $( ".to_fade" ).addClass( "fade" );
+function fromWelcomeToInstructions() {
+    $( ".main_section" ).animate({opacity: 0}, 300, function() {
+        $( ".main_section" ).remove();
+        showInstructions();
+    });
+
     $( ".to_slide_down" ).addClass( "slide_down" );
     $( ".to_shrink" ).addClass( "shrink" );
 
     $( ".button_text" ).animate({opacity: 0}, 200, function() {
         $( ".button_text" ).text("Fair enough");
-        $( ".button_text" ).animate({opacity: 1}, 200);
+        $( ".button_text" ).animate({opacity: 1}, 200, function() {
+            $( ".proceed_button" ).off().click(function() {
+                hideInstructions();
+            });
+        });
     });
-
-    setTimeout(function() {
-        $( ".to_fade" ).remove();
-        showInstructions();
-
-    }, 300);
 };
 
 function showInstructions() {
 
-    $( ".proceed_button" ).off().click(function() {
-        hideInstructions();
-    });
+    $( ".instructions_section" ).css({opacity: 1, display: "block"});
+    $( ".proceed_button" ).css("display", "block");
 
     $( ".to_show" ).css("opacity", "0");
 
     $( ".to_show" ).css("display", "block");
     $( ".to_show_flex" ).css("display", "flex");
 
+
     var elementsToShow = $( ".to_show" )
-    var flexElementsToShow = $( ".to_show_flex" )
 
     var elementsSequence = [
         [elementsToShow[2]],
         [elementsToShow[0], elementsToShow[1], elementsToShow[3]],
         [elementsToShow[4], elementsToShow[5]],
         [elementsToShow[6], elementsToShow[7], elementsToShow[8], elementsToShow[9]],
-        [elementsToShow[10], elementsToShow[11]]
+        [elementsToShow[10], elementsToShow[11]],
+        [$( ".proceed_button" )]
     ]
 
     for (let i = 0; i < elementsSequence.length; i++) {
@@ -60,4 +68,11 @@ function hideInstructions() {
         $( ".collage_section" ).css("display", "flex");
         $( ".collage_section" ).animate({opacity: 1}, 300);
     })
+}
+
+function hideCollage() {
+    $( ".collage_section" ).animate({opacity: 0}, 300, function () {
+        $( ".collage_section" ).css("display", "none");
+        showInstructions();
+    });
 }
