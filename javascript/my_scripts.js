17d7bf8f82d23b3735feb8ef5d42b3cb8d067652
Szymon Sentkowski <szysen123@gmail.com>
2019-02-07 08:03:50 +0100

    Further tests

--- javascript/my_scripts.js
+++ javascript/my_scripts.js
@@ -346,15 +346,11 @@ function handleTouchStart(evt) {
         left: evt.touches[0].clientX,
         top: evt.touches[0].clientY,
     };
-    if (evt.target.nodeName != "LABEL") {
-        evt.preventDefault();
-    }
 }
 
 function handleTouchMove(evt) {
     lastSwipeTouch = evt;
     precedingTouch = null;
-    evt.preventDefault();
 }
 
 function handleTouchEnd(evt) {
@@ -372,9 +368,6 @@ function handleTouchEnd(evt) {
                 }
             }
         }
-    }
-    if (evt.target.nodeName != "LABEL") {
-        evt.preventDefault();
     }
 }
 
