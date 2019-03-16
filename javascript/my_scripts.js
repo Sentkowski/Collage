9477680ea272113209bad5c4e4c08c1677e4e2b6
Szymon Sentkowski <szysen123@gmail.com>
2019-03-16 02:02:25 +0100

    Added the EXIF awareness

--- javascript/my_scripts.js
+++ javascript/my_scripts.js
@@ -212,6 +212,7 @@ class Frame {
         $( this.classSelector ).children().animate({opacity: 1}, 300, () => {
             $( this.inputIDSelector ).change(function (evt) {
                 addPhoto(evt);
+                evt.stopImmediatePropagation();
             });
         });
     }
@@ -578,11 +579,22 @@ function addPhoto(evt) {
     let file = element.files[0];
     let img = document.createElement("img");
     let reader = new FileReader();
-    reader.onloadend = function() {
-         $( img ).css("animation-play-state", "running")
-         img.src = reader.result;
-         // Remove the spinner
-         $( img ).siblings().remove();
+    reader.onload = function(evt) {
+        getOrientation(file, function(orientation) {
+            if (orientation > 1) {
+                resetOrientation(evt.target.result, orientation, function(newSrc) {
+                    img.src = newSrc;
+                    $( img ).css("animation-play-state", "running")
+                    // Remove the spinner
+                    $( img ).siblings().remove();
+                })
+            } else {
+                img.src = evt.target.result;
+                $( img ).css("animation-play-state", "running")
+                // Remove the spinner
+                $( img ).siblings().remove();
+            }
+        });
     }
     reader.readAsDataURL(file);
     $(element).after(img);
@@ -784,4 +796,95 @@ function detectLayoutChange() {
 function findElementsIndex(markup) {
     let r = /\d+/;
     return markup.outerHTML.match(r)[0];
+}
+
+
+// From https://stackoverflow.com/a/40867559
+function resetOrientation(srcBase64, srcOrientation, callback) {
+  var img = new Image();
+
+  img.onload = function() {
+    var width = img.width,
+        height = img.height,
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext("2d");
+
+    // set proper canvas dimensions before transform & export
+    if (4 < srcOrientation && srcOrientation < 9) {
+      canvas.width = height;
+      canvas.height = width;
+    } else {
+      canvas.width = width;
+      canvas.height = height;
+    }
+
+    // transform context before drawing image
+    switch (srcOrientation) {
+      case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+      case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+      case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+      case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+      case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+      case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+      default: break;
+    }
+
+    // draw image
+    ctx.drawImage(img, 0, 0);
+
+    // export base64
+    callback(canvas.toDataURL());
+  };
+
+  img.src = srcBase64;
+};
+
+// From https://stackoverflow.com/a/32490603
+function getOrientation(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+
+        var view = new DataView(e.target.result);
+        if (view.getUint16(0, false) != 0xFFD8)
+        {
+            return callback(-2);
+        }
+        var length = view.byteLength, offset = 2;
+        while (offset < length)
+        {
+            if (view.getUint16(offset+2, false) <= 8) return callback(-1);
+            var marker = view.getUint16(offset, false);
+            offset += 2;
+            if (marker == 0xFFE1)
+            {
+                if (view.getUint32(offset += 2, false) != 0x45786966)
+                {
+                    return callback(-1);
+                }
+
+                var little = view.getUint16(offset += 6, false) == 0x4949;
+                offset += view.getUint32(offset + 4, little);
+                var tags = view.getUint16(offset, little);
+                offset += 2;
+                for (var i = 0; i < tags; i++)
+                {
+                    if (view.getUint16(offset + (i * 12), little) == 0x0112)
+                    {
+                        return callback(view.getUint16(offset + (i * 12) + 8, little));
+                    }
+                }
+            }
+            else if ((marker & 0xFF00) != 0xFF00)
+            {
+                break;
+            }
+            else
+            {
+                offset += view.getUint16(offset, false);
+            }
+        }
+        return callback(-1);
+    };
+    reader.readAsArrayBuffer(file);
 }
